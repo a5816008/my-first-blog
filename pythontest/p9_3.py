8c65e5cd6d477c4a1f987634bbaac5b09b0eff97
a5816008 <a5816008@aoyama.jp>
2018-06-18 18:16:04 +0900

    Change p9_3 to p9_4

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -3,9 +3,13 @@ steps = [6543, 7000, 8900, 10789, 3467, 11045, 5095]
 labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 num_bars = len(steps)
 positions = range(1, num_bars+1)
-plt.barh(positions, steps, align='center')
+plt.bar(positions, steps, align='center')
+#以下は変更前(p9_3)のコード
+#plt.barh(positions, steps, align='center')
 
-plt.yticks(positions, labels)
+plt.xticks(positions, positions)
+#以下は変更前(p9_3)のコード
+#plt.yticks(positions, labels)
 plt.xlabel('Steps')
 plt.ylabel('Day')
 plt.title('Number of steps walked')
